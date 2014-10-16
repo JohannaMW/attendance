5cd6669c08d091a3fbcf47751e9c1b9f8b0c06a8
Johanna <jweinsziehr@gmx.de>
2014-10-16 16:27:10 -0700

    initial commit

--- attendy_app/tests.py
+++ attendy_app/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from forms import TeacherForm
+from models import People
+from views import check_in
 
-# Create your tests here.
+
+class FormTest(TestCase):
+    def test_teacher_form(self):
+        form = TeacherForm(data= {'date': '2014-10-15'})
+        self.assertTrue(form.is_valid())
+
+        # resp = self.client.get('/teacher_home/')
+        # self.assertEqual(resp.status_code, 200)
+        # self.assertTrue('Students checked' in resp.context)
+
+
+# class FormTest(TestCase):
+#     def test_teacher_form(self):
+#         form = TeacherForm(data= {'date': '2014-10-15'})
+#         form.cleaned_data = {'date': '2014-10-15'}
+#         resp = self.client.get('/teacher_home/')
+#         self.assertEqual(resp.status_code, 200)
+#         self.assertTrue('Students checked' in resp.context)
+
+# class ViewsTest(TestCase):
+#
+#     def test_check_in(self):
+#         user = People.objects.create_user(username='test-user', email='test@test.com', password='password')
+#         check_in(user)
+#         resp = self.client.get('/check_in/')
+#         self.assertEqual(resp.status_code, 200)
+#         self.assertEqual(resp.context[0].fields.username, 'test-user')
+#
+#         pass
+#
+#     def test_get_mayor(self):
+#         pass
+
+
+
